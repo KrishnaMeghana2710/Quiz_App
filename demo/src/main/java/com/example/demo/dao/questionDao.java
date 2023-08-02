3f324e829209c43de9b77c6b5b6ff609844d9a86
meghana <krishnameghana2710@gmail.com>
2023-08-02 22:59:02 +0530

    removed unused imports

--- demo/src/main/java/com/example/demo/dao/questionDao.java
+++ demo/src/main/java/com/example/demo/dao/questionDao.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Question;
 
-
 public interface questionDao extends JpaRepository<Question,Integer>{
 
    Optional<Question> findByQuestion(String question);
